ff20bde41444e501ea38cd2783031e3f59fb82ff
Mauricio Terceros <mauricio.terceros@truexted.bo>
2018-04-10 11:53:04 -0400

    update tests

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -3,6 +3,6 @@ var Calculator = require('../js/1.js');
 describe('Addition of two numbers', function() {
 	var calculator = new Calculator();
 	it ('add two numbers', function(){
-		expect(calculator.addition(3,4)).toEqual(8);
+		expect(calculator.addition(3,4)).toEqual(7);
 	});
 });
